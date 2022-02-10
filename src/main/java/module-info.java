70ba1dd9d66b580b43c8b70223ede968e0e305b1
João Marcus <joaomarcus12769@gmail.com>
2022-02-10 20:20:52 -0300

    init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.ifce.ppd.tsoroyematatu {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.ifce.ppd.tsoroyematatu to javafx.fxml;
+    exports com.ifce.ppd.tsoroyematatu;
+}
