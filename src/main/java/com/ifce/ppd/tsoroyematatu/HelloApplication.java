2419acfc2c5d31d536af8ec57abb92ac44912465
João Marcus <joaomarcus12769@gmail.com>
2022-02-10 23:28:12 -0300

    11 -> 17 | layout de jogo

--- src/main/java/com/ifce/ppd/tsoroyematatu/HelloApplication.java
+++ src/main/java/com/ifce/ppd/tsoroyematatu/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 967, 791);
         stage.setTitle("Tsoro Yematatu - João Marcus Maia Rocha - IFCE - PPD");
         stage.setScene(scene);
         stage.show();
