e45a65c52a1742c9f61197597f5085b1cb3a4993
João Marcus <joaomarcus12769@gmail.com>
2022-02-18 11:04:36 -0300

    client model

---
+++ src/main/java/com/ifce/ppd/tsoroyematatu/exceptions/NoClientModelException.java
@@ -0,0 +1,13 @@
+package com.ifce.ppd.tsoroyematatu.exceptions;
+
+/**
+ *
+ */
+public class NoClientModelException extends Exception {
+    /**
+     *
+     */
+    public NoClientModelException() {
+        super("Não há modelo de cliente!");
+    }
+}
