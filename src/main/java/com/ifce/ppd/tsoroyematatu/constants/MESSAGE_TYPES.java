ad53213081e5ff66f2842a2be19c2b2ad59ee4ca
João Marcus <joaomarcus12769@gmail.com>
2022-02-26 00:06:32 -0300

    add is first player

--- src/main/java/com/ifce/ppd/tsoroyematatu/constants/MESSAGE_TYPES.java
+++ src/main/java/com/ifce/ppd/tsoroyematatu/constants/MESSAGE_TYPES.java
@@ -6,9 +6,10 @@ public enum MESSAGE_TYPES {
     MOVE(2),
     WAIT_RIVAL_CONNECT(3),
     WAIT_RIVAL_MAKE_MOVE(4),
-    CAN_PLAY(5),
+    CAN_MAKE_MOVE(5),
     PLAYABLE(6),
-    EXIT(7);
+    EXIT(7),
+    IS_FIRST_PLAYER(8);
 
     private final int flag;
 
