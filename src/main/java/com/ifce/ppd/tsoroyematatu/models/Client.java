b1fc431062e771e41531a8a3e0e8499a05191901
João Marcus <joaomarcus12769@gmail.com>
2022-03-01 19:18:23 -0300

    doc

--- src/main/java/com/ifce/ppd/tsoroyematatu/models/Client.java
+++ src/main/java/com/ifce/ppd/tsoroyematatu/models/Client.java
@@ -5,20 +5,33 @@ import java.io.Serializable;
 public class Client implements Serializable {
     private final String name;
     private int id;
-    private String roomId;
 
     public Client(String name) {
         this.name = name;
     }
 
+    /**
+     * ID's getter.
+     *
+     * @return The id of the client.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Id's setter.
+     * @param id ID to be set.
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Name's getter.
+     *
+     * @return The name of the client.
+     */
     public String getName() {
         return name;
     }
