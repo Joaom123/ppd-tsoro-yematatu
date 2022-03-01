58b796259219de9fe7b707749796ffdebe40d34c
João Marcus <joaomarcus12769@gmail.com>
2022-03-01 16:31:12 -0300

    doc

--- src/main/java/com/ifce/ppd/tsoroyematatu/client/ServerConnection.java
+++ src/main/java/com/ifce/ppd/tsoroyematatu/client/ServerConnection.java
@@ -12,14 +12,11 @@ import java.net.Socket;
  */
 public class ServerConnection {
     private Controller currentController;
-    private Socket socket;
     private Client clientModel;
     private String roomId;
     private String hostname;
     private SendThread sendThread;
-    private ReceiveThread receiveThread;
     private boolean isConnected = false;
-    private boolean isPlayable = false;
     private boolean isFirstPlayer = false;
     private int turn = 0;
 
@@ -35,9 +32,9 @@ public class ServerConnection {
      */
     public void startConnection(String host) {
         try {
-            socket = new Socket(host, 12345);
+            Socket socket = new Socket(host, 12345);
 
-            receiveThread = new ReceiveThread(socket, this);
+            ReceiveThread receiveThread = new ReceiveThread(socket, this);
             sendThread = new SendThread(socket, this);
 
             receiveThread.start();
@@ -51,6 +48,8 @@ public class ServerConnection {
     }
 
     /**
+     * Send init flag.
+     *
      * @throws NullClientException Lançada quando não há cliente.
      */
     public void createClientOnServer(String roomId) throws NullClientException {
@@ -58,47 +57,97 @@ public class ServerConnection {
         sendThread.sendInitFlag(roomId);
     }
 
+    /**
+     * Client's getter.
+     *
+     * @return The Client.
+     */
     public Client getClient() {
         return clientModel;
     }
 
+    /**
+     * Client's Setter.
+     * @param clientModel Client to be set.
+     */
     public void setClient(Client clientModel) {
         this.clientModel = clientModel;
     }
 
+    /**
+     * FirstPlayer's getter.
+     *
+     * @return FirstPlayer
+     */
     public boolean isFirstPlayer() {
         return isFirstPlayer;
     }
 
+    /**
+     * FirstPlayer's setter.
+     * @param firstPlayer FirstPlayer to be set.
+     */
     public void setFirstPlayer(boolean firstPlayer) {
         isFirstPlayer = firstPlayer;
     }
 
+    /**
+     * RoomId's getter.
+     *
+     * @return RoomId.
+     */
     public String getRoomId() {
         return roomId;
     }
 
+    /**
+     * RoomId's setter.
+     * @param roomId RoomId to be set.
+     */
     public void setRoomId(String roomId) {
         this.roomId = roomId;
     }
 
+    /**
+     * Hostname's getter.
+     *
+     * @return Hostname.
+     */
     public String getHostname() {
         return hostname;
     }
 
+    /**
+     * Hostname's setter.
+     * @param hostname Hostname to be set.
+     */
     public void setHostname(String hostname) {
         this.hostname = hostname;
     }
 
+    /**
+     * Turn's getter.
+     *
+     * @return Turn number.
+     */
     public int getTurn() {
         return turn;
     }
 
+    /**
+     * Setter of turn. Update the turn on the controller.
+     *
+     * @param turn Turn number.
+     */
     public void setTurn(int turn) {
         this.turn = turn;
         currentController.updateTurn();
     }
 
+    /**
+     * CurrectController's setter.
+     * @param currentController The controller to be set.
+     */
     public void setCurrentController(Controller currentController) {
         this.currentController = currentController;
     }
@@ -142,64 +191,120 @@ public class ServerConnection {
         currentController.receiveMove(pieceId, pointId);
     }
 
+    /**
+     * Execute goToGame in controller.
+     */
     public void goToGame() {
-        isPlayable = true;
         currentController.goToGame();
     }
 
+    /**
+     * Send exit flag.
+     */
     public void sendExit() {
         sendThread.sendExit();
     }
 
+    /**
+     * Execute waitRivalMakeMove in controller.
+     */
     public void waitRivalMakeMove() {
         currentController.waitRivalMakeMove();
     }
 
+    /**
+     * Execute canMakeMove in controller.
+     */
     public void canMakeMove() {
         currentController.canMakeMove();
     }
 
+    /**
+     * Execute winner fuction in controller.
+     */
     public void winner() {
         System.out.println("Venceu");
         currentController.winner();
     }
 
+    /**
+     * Execute loser funtion in controller.
+     */
     public void loser() {
         System.out.println("Perdeu");
         currentController.loser();
     }
 
+    /**
+     * Send withdrawal flag.
+     */
     public void sendWithdrawalFlag() {
         sendThread.sendWithdrawalFlag();
     }
 
+    /**
+     * Execute resetGame in controller. Set turn to 0. Change firstPlayer.
+     */
     public void resetGame() {
         turn = 0;
         isFirstPlayer = !isFirstPlayer;
         currentController.resetGame();
     }
 
+    /**
+     * Send draw flag.
+     */
     public void sendDrawFlag() {
         sendThread.sendDrawFlag();
     }
 
+    /**
+     * Execute drawConfirmation in controller.
+     */
     public void drawConfirmation() {
         currentController.drawConfirmation();
     }
 
+    /**
+     * Send draw denied flag.
+     */
     public void sendDrawDeniedFlag() {
         sendThread.sendDrawDeniedFlag();
     }
 
+    /**
+     * Send draw accepted flag.
+     */
     public void sendDrawAcceptedFlag() {
         sendThread.sendDrawAcceptedFlag();
     }
 
+    /**
+     * Execute drawAccpeted in controller.
+     */
     public void drawAccepted() {
         currentController.drawAccepted();
     }
 
+    /**
+     * Execute drawDenied in controller.
+     */
     public void drawDenied() {
         currentController.drawDenied();
+    }
+
+    /**
+     * Execute exit in controller.
+     */
+    public void exit() {
+        // destroy connection
+        currentController.exit();
+    }
+
+    /**
+     * Execute roomIsFull in controller.
+     */
+    public void roomIsFull() {
+        currentController.roomIsFull();
     }
 }
