429cbb3266b9cf87b3a814bcf5f4b3147ed401e9
João Marcus <joaomarcus12769@gmail.com>
2022-03-01 16:09:05 -0300

    doc

--- src/main/java/com/ifce/ppd/tsoroyematatu/controllers/Controller.java
+++ src/main/java/com/ifce/ppd/tsoroyematatu/controllers/Controller.java
@@ -25,30 +25,69 @@ public abstract class Controller {
     public void receiveMove(String selectedPieceId, String selectedPointId) {
     }
 
+    /**
+     * Disable pieces of the player. So the player must wait to rival make move.
+     */
     public void waitRivalMakeMove() {
     }
 
+    /**
+     * After the rival makes his move, the player is released to make his move.
+     */
     public void canMakeMove() {
     }
 
+    /**
+     * Update turn number.
+     */
     public void updateTurn() {
     }
 
+    /**
+     * Show loser message.
+     */
     public void loser() {
     }
 
+    /**
+     * Show winner message.
+     */
     public void winner() {
     }
 
+    /**
+     * Reset the game's turn, pieces and points.
+     */
     public void resetGame() {
     }
 
+    /**
+     * Show draw confirmation dialog with options 'Yes' and 'No'.
+     */
     public void drawConfirmation() {
     }
 
+    /**
+     * Show dialog with accpeted draw message.
+     */
     public void drawAccepted() {
     }
 
+    /**
+     * Show dialog with denied draw message.
+     */
     public void drawDenied() {
+    }
+
+    /**
+     * Exit the game. Go to init view.
+     */
+    public void exit() {
+    }
+
+    /**
+     * Show 'room is full' dialog.
+     */
+    public void roomIsFull() {
     }
 }
