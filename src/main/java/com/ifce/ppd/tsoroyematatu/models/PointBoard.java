df9e959db537fd7c77796d149ec04585096817e5
João Marcus <joaomarcus12769@gmail.com>
2022-03-01 19:10:48 -0300

    doc

--- src/main/java/com/ifce/ppd/tsoroyematatu/models/PointBoard.java
+++ src/main/java/com/ifce/ppd/tsoroyematatu/models/PointBoard.java
@@ -1,7 +1,5 @@
 package com.ifce.ppd.tsoroyematatu.models;
 
-import com.ifce.ppd.tsoroyematatu.models.PieceBoard;
-
 public class PointBoard {
     private final String id;
     private PieceBoard pieceBoard = null;
@@ -10,18 +8,36 @@ public class PointBoard {
         this.id = id;
     }
 
+    /**
+     * Id's getter.
+     *
+     * @return Id value
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * PieceBoard's getter.
+     *
+     * @return The pieceBoard.
+     */
     public PieceBoard getPieceBoard() {
         return pieceBoard;
     }
 
+    /**
+     * PieceBoard's setter.
+     *
+     * @param pieceBoard Piece board to be set.
+     */
     public void setPieceBoard(PieceBoard pieceBoard) {
         this.pieceBoard = pieceBoard;
     }
 
+    /**
+     * @return True if is occupied by a pieceBoard. False if isn't occupied.
+     */
     public boolean isOccupied() {
         return pieceBoard != null;
     }
